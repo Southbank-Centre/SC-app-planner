a38a6e50ad0b01fafa513ea8318b595b861951a2
samueljmurray <samueljmurray@gmail.com>
2015-03-12 12:07:40 +0000

    initial commit

---
+++ app/SC-app-planner.module.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name SC-app-planner
+ * @description
+ *
+ * Provides the app with the ability to display planner content and features
+ */
+angular
+  .module('SC-app-planner');
